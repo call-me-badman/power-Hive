a2c081813ea7e14eb594e0ec2346ea067b11699b
Irakoze Olga <irakozeolga490@gmail.com>
2026-02-28 20:24:03 +0200

    Defined the structure of the ideal codebase

--- BACK-END/controller/chat.controller.js
+++ BACK-END/controller/chat.controller.js
@@ -1,7 +1,6 @@
 import Message from "../models/message.model.js";
 import { io } from "../app.js";
 
-// Call this once in app.js or server entry point, not as a route handler
 export const initSocket = () => {
     io.on("connection", (socket) => {
         console.log("a user connected", socket.id);
